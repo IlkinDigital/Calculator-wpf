12a48ca57554f3c88c66a26531659af4a3be975c
Ilkin <lester54539@gmail.com>
2022-02-22 00:22:24 +0400

    Add project files.

---
+++ Models/IOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+    public interface IOperation<Ty>
+    {
+        public Ty Item1 { get; set; }
+        public Ty Item2 { get; set; }
+
+        public Ty Operate();
+    }
+}
